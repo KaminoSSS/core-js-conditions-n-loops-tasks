c34675001ede7e2321af45abfa0954b6edec10f1
Egor Perfiliev <perfilev.egor2016@gmail.com>
2023-12-31 15:19:30 +0300

    feat: implement getMaxNumber function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,16 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let result;
+  if (a >= b && a >= c) {
+    result = a;
+  } else if (b >= a && b >= c) {
+    result = b;
+  } else {
+    result = c;
+  }
+  return result;
 }
 
 /**
